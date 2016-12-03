1a8f61fd1ae872f654da134dc7648f8e55d1f4e6
Ekaterina Ivanova <ekaterina.ivanova@saltpepper.ru>
2016-12-03 21:33:16 +0300

    Change message

--- face-detection-node-opencv-master/server/UdpClient.js
+++ face-detection-node-opencv-master/server/UdpClient.js
@@ -1,13 +1,13 @@
 const dgram = require('dgram');
 
 class UdpClient {
-  constructor(port = 9082, host = '127.0.0.1') {
+  constructor(port = 55056, host = 'localhost') {
     this.port = port;
     this.host = host;
     this.client = dgram.createSocket('udp4');
   }
   send(message, errHandler) {
-    this.client.send(message.toString(), this.port, this.host);
+    this.client.send((message >>> 0).toString(2), this.port, this.host);
   }
 };
 
