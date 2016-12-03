56e28c4e771c62e0ae31c4fe6f28ffc02e85970c
Ekaterina Ivanova <ekaterina.ivanova@saltpepper.ru>
2016-12-03 12:16:40 +0300

    Add face detection

---
+++ face-detection-node-opencv-master/server/lib/routes/socket.js
@@ -0,0 +1,64 @@
+var cv = require('opencv');
+
+// camera properties
+var camWidth = 320;
+var camHeight = 240;
+var camFps = 10;
+var camInterval = 1000 / camFps;
+
+// smile detection properties
+var color = [0, 255, 0];
+var cascadeFile = './data/haarcascade_smile.xml';
+
+// initialize camera
+var camera = new cv.VideoCapture(0);
+camera.setWidth(camWidth);
+camera.setHeight(camHeight);
+
+
+module.exports = function (socket) {
+  setInterval(function() {
+    camera.read(function(err, im) {
+      if (err) throw err;
+	  smileCascade = new cv.CascadeClassifier(cascadeFile);
+	  smileCascade.detectMultiScale(im, function(err, smiles) {
+	  	console.log(smiles);
+	  	  for (var k = 0; k < smiles.length; k++) {
+	  	 	if (err) throw err;
+            var smile = smiles[k];
+			im.rectangle([smile.x, smile.y], [smile.width, smile.height], color, 2);
+		  }
+		  socket.emit('frame', { buffer: im.toBuffer() });
+		},6, 16
+      );
+
+        
+      });
+  }, camInterval);
+};
+
+
+
+/*module.exports = function (socket) {
+  setInterval(function() {
+    camera.read(function(err, im) {
+      if (err) throw err;
+
+      im.detectObject('./data/haarcascade_smile.xml', {}, function(err, faces) {
+        if (err) throw err;
+
+        for (var i = 0; i < faces.length; i++) {
+          face = faces[i];
+                    console.log(face);
+          if(face.x > 90 && face.x < 120 && face.y > 150){
+          
+          im.rectangle([face.x, face.y], [face.width, face.height], rectColor, rectThickness);
+			}
+        }
+
+        socket.emit('frame', { buffer: im.toBuffer() });
+      });
+    });
+  }, camInterval);
+};
+*/
